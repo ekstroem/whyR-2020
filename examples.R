f35c00f4137ba2d32d73c7b1a90fbbc703f79bbe
AHP <ahpe@sund.ku.dk>
2020-12-03 18:12:47 +0100

    annes slides

---
+++ examples.R
@@ -0,0 +1,10 @@
+library(dataReporter)
+data(presidentData)
+
+# Example 
+# - html output
+# - No summaries
+# - Use tableVisual for character variables
+# - Only 5 problematic values from checks
+# - Treat Name as character
+# - Report title: Dirty Presidents
